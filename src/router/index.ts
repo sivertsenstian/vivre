6b8294bbdaa7bd5c3f9dc8e736316cef8de46b69
Stian Sivertsen <sivertsenstian@gmail.com>
2024-04-24 08:42:28 +0200

    Fix router issues

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import VivreView from '@/views/VivreView.vue'
-import SettingsView from '@/views/SettingsView.vue'
-import CreepRoutesView from '@/views/CreepRoutesView.vue'
-import BuildOrderView from '@/views/BuildOrderView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import VivreView from "@/views/VivreView.vue";
+import SettingsView from "@/views/SettingsView.vue";
+import CreepRoutesView from "@/views/CreepRoutesView.vue";
+import BuildOrderView from "@/views/BuildOrderView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: VivreView
+      path: "/",
+      name: "home",
+      component: VivreView,
     },
     {
-      path: '/creeproutes',
-      name: 'creeproutes',
-      component: CreepRoutesView
+      path: "/creeproutes",
+      name: "creeproutes",
+      component: CreepRoutesView,
     },
     {
-      path: '/buildorders',
-      name: 'buildorders',
-      component: BuildOrderView
+      path: "/buildorders",
+      name: "buildorders",
+      component: BuildOrderView,
     },
     {
-      path: '/settings',
-      name: 'settings',
-      component: SettingsView
-    }
-  ]
-})
+      path: "/settings",
+      name: "settings",
+      component: SettingsView,
+    },
+  ],
+});
 
-export default router
+export default router;
