78ad30cd30a14e6ad8e805ee31a3de524f561d77
Stian Sivertsen <sivertsenstian@gmail.com>
2024-06-05 22:44:03 +0200

    Add help and ST for all except ud

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { useSettingsStore } from '@/stores/settings'
-const settings = useSettingsStore()
+import help from "@/assets/halp.jpg";
+import { useSettingsStore } from "@/stores/settings";
+const settings = useSettingsStore();
 </script>
 
 <template>
@@ -15,7 +16,19 @@ const settings = useSettingsStore()
       </v-row>
       <v-row>
         <v-col cols="12">
-          <v-text-field label="Weekly Ladder Goal" v-model.trim="settings.data.goal" clearable />
+          <v-text-field
+            label="Weekly Ladder Goal"
+            v-model.trim="settings.data.goal"
+            clearable
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="10">
+          <div class="text-h4">What does this all mean?</div>
+        </v-col>
+        <v-col cols="10">
+          <img style="border: 1px solid white" :src="help" width="100%" />
         </v-col>
       </v-row>
     </v-container>
