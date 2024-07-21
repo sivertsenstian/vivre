e83277976913c0566050d91b93074119231577ab
Stian Sivertsen <sivertsenstian@gmail.com>
2024-07-21 13:51:03 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import * as path from 'path'
-import { fileURLToPath, URL } from 'node:url'
+import * as path from "path";
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vivre/',
+  base: "/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@assets': path.resolve(__dirname, 'src/assets')
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@assets": path.resolve(__dirname, "src/assets"),
+    },
+  },
+});
