1105c9c8310cd56865ce5be8740fdadf8ed9ebb5
Stian Sivertsen <sivertsenstian@gmail.com>
2024-11-22 01:46:06 +0100

    Fix more stuff!

--- src/components/ResultChart.vue
+++ src/components/ResultChart.vue
@@ -16,13 +16,10 @@ const isDark = computed(() => theme.global.current.value.dark);
 </script>
 
 <template>
-  <div
-    v-if="
-      !_isNil(result?.total) && !_isNil(result?.wins) && !_isNil(result?.loss)
-    ">
+  <div v-if="!_isNil(result?.total) && result.total > 0">
     <v-progress-linear
       color="green-lighten-1"
-      :model-value="(result.wins / result.total) * 100"
+      :model-value="((result.wins ?? 0) / result.total) * 100"
       :height="height"
       :style="{ top: height / 2 + 'px', border: '1px solid green' }">
       <template v-slot:default="{ value }">
@@ -30,7 +27,7 @@ const isDark = computed(() => theme.global.current.value.dark);
           v-if="result.wins"
           class="text-white text-caption font-weight-bold"
           :style="{
-            marginRight: (result.loss / result.total) * 100 + '%',
+            marginRight: ((result.loss ?? 0) / result.total) * 100 + '%',
           }"
           >{{ result.wins }}W
           <span v-if="percentage" class="text-caption"
@@ -41,7 +38,7 @@ const isDark = computed(() => theme.global.current.value.dark);
     </v-progress-linear>
     <v-progress-linear
       color="red-lighten-1"
-      :model-value="(result.loss / result.total) * 100"
+      :model-value="((result.loss ?? 0) / result.total) * 100"
       :height="height"
       :style="{
         top: -height / 2 + 'px',
@@ -52,7 +49,7 @@ const isDark = computed(() => theme.global.current.value.dark);
         v-if="result.loss"
         class="text-white text-caption font-weight-bold"
         :style="{
-          marginLeft: (result.wins / result.total) * 100 + '%',
+          marginLeft: ((result.wins ?? 0) / result.total) * 100 + '%',
           color: 'lime',
         }">
         {{ result.loss }}L</span
