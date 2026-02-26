9b63326483c3511155ea23384809780fd16b5353
Stian Sivertsen <sivertsenstian@gmail.com>
2026-02-26 15:31:55 +0100

    Improve inventory

--- src/games/hotkeystorm/components/InventoryCommand.vue
+++ src/games/hotkeystorm/components/InventoryCommand.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { getIconUrl } from '@/utilities/api.ts';
-import { computed } from 'vue';
+import { computed, onMounted, onUpdated, ref } from 'vue';
 import { useHotKeyStormStore } from '@/games/hotkeystorm/store.ts';
+import _sample from 'lodash/sample';
 
 const store = useHotKeyStormStore();
 
@@ -18,10 +19,43 @@ const active = computed(
     store.getHotkeyFromAction(props.name, props.action)?.toLowerCase() !==
     props.current?.toLowerCase(),
 );
+
+const items = [
+  'claws of attack',
+  'ring of protection',
+  'ring of regeneration',
+  'mantle of intelligence',
+  'boots of speed',
+  'gloves of haste',
+  'circlet of nobility',
+  'helm of valor',
+  'hood of cunning',
+  'medalion of courage',
+  'pendant of energy',
+  'pendant of mana',
+  'periapt of vitality',
+];
+const randomIcon = () => {
+  return Math.random() < 0.25
+    ? getIconUrl(_sample(items) ?? 'claws of attack')
+    : '/empty_inventory.jpg';
+};
+
+const filler = ref<any>({});
+onMounted(() => {
+  filler.value = {
+    Item1: randomIcon(),
+    Item2: randomIcon(),
+    Item3: randomIcon(),
+    Item4: randomIcon(),
+    Item5: randomIcon(),
+    Item6: randomIcon(),
+  };
+});
 </script>
 
 <template>
-  <v-table class="pa-0 mx-3">
+  <v-table class="pa-0 mx-3" :key="`${name}_${action}`">
     <tbody>
       <tr>
         <td
@@ -31,12 +65,14 @@ const active = computed(
             v-if="name === 'Item1'"
             :src="getIconUrl(action)"
             :width="size"
+            class="blink"
             :style="{
               filter: active ? 'brightness(1.0)' : 'brightness(1.5)',
             }" />
           <v-img
             v-else
-            src="/empty_inventory.jpg"
+            :src="filler['Item1']"
+            style="filter: brightness(0.75)"
             :width="size"
             :height="size" />
         </td>
@@ -47,12 +83,14 @@ const active = computed(
             v-if="name === 'Item2'"
             :src="getIconUrl(action)"
             :width="size"
+            class="blink"
             :style="{
               filter: active ? 'brightness(1.0)' : 'brightness(1.5)',
             }" />
           <v-img
             v-else
-            src="/empty_inventory.jpg"
+            :src="filler['Item2']"
+            style="filter: brightness(0.75)"
             :width="size"
             :height="size" />
         </td>
@@ -65,12 +103,14 @@ const active = computed(
             v-if="name === 'Item3'"
             :src="getIconUrl(action)"
             :width="size"
+            class="blink"
             :style="{
               filter: active ? 'brightness(1.0)' : 'brightness(1.5)',
             }" />
           <v-img
             v-else
-            src="/empty_inventory.jpg"
+            :src="filler['Item3']"
+            style="filter: brightness(0.75)"
             :width="size"
             :height="size" />
         </td>
@@ -81,12 +121,14 @@ const active = computed(
             v-if="name === 'Item4'"
             :src="getIconUrl(action)"
             :width="size"
+            class="blink"
             :style="{
               filter: active ? 'brightness(1.0)' : 'brightness(1.5)',
             }" />
           <v-img
             v-else
-            src="/empty_inventory.jpg"
+            :src="filler['Item4']"
+            style="filter: brightness(0.75)"
             :width="size"
             :height="size" />
         </td>
@@ -95,16 +137,47 @@ const active = computed(
         <td
           :style="`border: 1px solid black; height: ${size}px; width: ${size}px`"
           class="pa-0 ma-0">
-          <v-img src="/empty_inventory.jpg" :width="size" :height="size" />
+          <v-img
+            v-if="name === 'Item5'"
+            :src="getIconUrl(action)"
+            :width="size"
+            class="blink"
+            :style="{
+              filter: active ? 'brightness(1.0)' : 'brightness(1.5)',
+            }" />
+          <v-img
+            :src="filler['Item5']"
+            style="filter: brightness(0.75)"
+            :width="size"
+            :height="size"
+            v-else />
         </td>
         <td
           :style="`border: 1px solid black; height: ${size}px; width: ${size}px`"
           class="pa-0 ma-0">
-          <v-img src="/empty_inventory.jpg" :width="size" :height="size" />
+          <v-img
+            v-if="name === 'Item6'"
+            :src="getIconUrl(action)"
+            :width="size"
+            class="blink"
+            :style="{
+              filter: active ? 'brightness(1.0)' : 'brightness(1.5)',
+            }" />
+          <v-img
+            :src="filler['Item6']"
+            style="filter: brightness(0.75)"
+            :width="size"
+            :height="size"
+            v-else />
         </td>
       </tr>
     </tbody>
   </v-table>
 </template>
 
-<style scoped></style>
+<style scoped>
+.blink {
+  box-shadow: 0 0 2px 2px gold !important;
+  animation: pulse 2s infinite;
+}
+</style>
